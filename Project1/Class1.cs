d9ed8194ec6b268a15cd71083c224bfc4120a5cc
davidsuran <surandavid@gmail.com>
2020-03-21 11:49:06 +0100

    Added template.

---
+++ Project1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace $safeprojectname$
+{
+    public class Class1
+    {
+        public bool ReturnTrue()
+        {
+            return true;
+        }
+    }
+}
---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Project1;
+using System;
+using Xunit;
+
+
+namespace $safeprojectname$
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(new Class1().ReturnTrue());
+        }
+    }
+}
